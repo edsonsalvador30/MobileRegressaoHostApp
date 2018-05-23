1b9e583eaf5c92e2861fc404d6345c05fe0815f4
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-05-23 14:13:41 +0000

    Primeiro Commit

---
+++ src/main/mobileapp/www/js/blockly/teste.blockly.js
@@ -0,0 +1,11 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Teste = window.blockly.js.blockly.Teste || {};
+
+/**
+ * teste
+ */
+window.blockly.js.blockly.Teste.Executar = function() {
+	return 'teste';
+}
---
+++ src/main/mobileapp/www/js/hostApp.js
@@ -0,0 +1,1 @@
+window.hostApp = 'https://12-181-10010.debug.ide.cronapp.io/';
